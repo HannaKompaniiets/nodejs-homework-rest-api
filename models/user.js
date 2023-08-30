d0589c7d3f98ab199d714dcce6c74fe9d812b47c
Hanna Kompaniiets <anna.komp24@gmail.com>
2023-08-30 14:09:20 +0200

    hw5

--- models/user.js
+++ models/user.js
@@ -13,6 +13,10 @@ const userSchema = new Schema(
       required: [true, "Email is required"],
       unique: true,
     },
+    avatarURL: {
+      type: String,
+      required: true,
+    },
     subscription: {
       type: String,
       enum: ["starter", "pro", "business"],
@@ -45,8 +49,6 @@ const addLoginSchema = Joi.object({
     .messages({ "any.required": "missing required password field" }),
 });
 
-
-
-const schemas = { addLoginSchema, addRegisterSchema};
+const schemas = { addLoginSchema, addRegisterSchema };
 
 module.exports = { User, schemas };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const ctrl = require("../../controllers/auth");
 
 const { schemas } = require("../../models/user");
-const { authenticate, validatedBody } = require("../../middleware");
+const { authenticate, validatedBody, upload } = require("../../middleware");
 
 router.post(
   "/register",
@@ -13,5 +13,11 @@ router.post(
 router.post("/login", validatedBody(schemas.addLoginSchema), ctrl.login);
 router.get("/current", authenticate, ctrl.getCurrent);
 router.post("/logout", authenticate, ctrl.logout);
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  ctrl.updateAvatar
+);
 
 module.exports = router;
